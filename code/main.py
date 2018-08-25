7685e25caee304bce7c414101b2bc5c57cada283
danielevezz <danielevezz@gmail.com>
2018-08-25 16:31:17 +0200

    Matches

--- code/main.py
+++ code/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from bs4 import BeautifulSoup as bs
 import requests
 import re
@@ -8,7 +7,7 @@ url = "https://www.fantagazzetta.com/probabili-formazioni-serie-a"
 
 page = requests.get(url, headers={"User-Agent": "Requests"}).content
 soup = bs(page, "html.parser")
-
+print("Request ok")
 complete = {
     "Ata": "Atalanta",
     "Bol": "Bologna",
@@ -34,8 +33,30 @@ complete = {
 with open("team.csv", "r") as file:
     team = csv.reader(file, delimiter=",")
 
+    # Print the matches
+    print("Le partite sono: \n")
+    for link in soup.find_all("a"):
+        href = link.get("href")
+
+        matches = re.findall(r'[a-z]+-[a-z]+', href)
+        for match in matches:
+            if match is not None:
+                tmp = match.split("-")
+                home = tmp[0].capitalize()
+                away = tmp[1].capitalize()
+                if home == "Spal":
+                    home = home.upper()
+                if away == "Spal":
+                    away = away.upper()
+                if home[:3] in complete and away[:3] in complete:
+                    print(f"{home} - {away}")
+
+    print("")
+    # Search the player percentages
     for player in team:
-        nome = player[0].strip().replace(" ","-")
+        if team is None:
+            continue
+        nome = player[0].strip().replace(" ", "-")
         squadra = complete[player[1]].strip()
 
         for link in soup.find_all("a"):
@@ -47,19 +68,17 @@ with open("team.csv", "r") as file:
                 if sibling is None:
                     sibling = link.parent.previousSibling
                     panchina = True
-            
-                
+
                 sibling = str(sibling)
 
-                perc = re.search(r'[0-9]+%',sibling).group()
+                perc = re.search(r'[0-9]+%', sibling).group()
 
-                nome = nome.replace("-"," ").capitalize()
+                nome = nome.replace("-", " ").capitalize()
 
                 if panchina:
                     print(f"{nome} è in panchina con indice di subentro: {perc}")
                 else:
                     print(f"{nome}: {perc}")
-        
+
         if f"{nome.capitalize()}:" in str(soup):
             print(f"{nome} non è disponibile")
-
